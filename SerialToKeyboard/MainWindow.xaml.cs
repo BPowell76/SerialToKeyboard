910ab23329f9550ea1c4ab13420168d9d4c5adca
Brian Powell Jr <b.powell@techmet-carbide.com>
2025-03-11 08:49:33 -0400

    Add combobox for serial port selection. Add startup logic to populate combobox and set default baud rate.

--- SerialToKeyboard/MainWindow.xaml.cs
+++ SerialToKeyboard/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Windows;
 
 namespace SerialToKeyboard
 {
@@ -7,9 +10,24 @@ namespace SerialToKeyboard
     /// </summary>
     public partial class MainWindow
     {
+        private List<string> _serialPorts;
+        
         public MainWindow()
         {
             InitializeComponent();
+            _serialPorts = new List<string>();
+            string[] ports = SerialPort.GetPortNames();
+            foreach (string port in ports)
+            {
+                _serialPorts.Add(port);
+            }
+            _serialPorts.Sort();
+
+            cmbCOMPorts.ItemsSource = _serialPorts;
+            if (cmbCOMPorts.Items.Count > 0)
+                cmbCOMPorts.SelectedIndex = 0;
+            
+            radBtn115200.IsChecked = true;
         }
 
         private void BtnEnable_OnClick(object sender, RoutedEventArgs e)
